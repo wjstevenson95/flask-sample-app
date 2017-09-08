fe50910cec535ded07f71f45d2ed1f8c2a393e8a
William Stevenson <wstevenson@169-231-95-18.wireless.ucsb.edu>
2017-09-08 00:00:14 -0700

    might have gotten basic login working

--- webapp.py
+++ webapp.py
@@ -43,7 +43,7 @@ def authorized():
 		github = OAuth2Session(client_id, state=session['oauth_state'], redirect_uri=redirect_uri)
 		token = github.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
 		session['oauth_token'] = token
-		return redirect(url_for('render_home'))
+		return redirect(url_for('profile'))
 
 
 @app.route('/profile', methods=["GET"])
