3eddcfaaaf53d8c8ab7abae140b874bfa1f0f726
William Stevenson <wstevenson@169-231-95-18.wireless.ucsb.edu>
2017-09-07 22:34:32 -0700

    testing...

--- webapp.py
+++ webapp.py
@@ -46,7 +46,7 @@ def authorized():
 		github = OAuth2Session(client_id, state=session['oauth_state'])
 		token = github.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
 
-		session['oauth_token']= token
+		session['oauth_token'] = token
 		return redirect(url_for('.profile'))
 	except KeyError as error:
 		session.clear()
